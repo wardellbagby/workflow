ede1bf49ec56dfdce7a4b8e5f0900f8721cce7f0
Timothy Donnelly <tim@tdonnelly.com>
2019-03-16 15:06:01 -0700

    Update ReactiveSwift to 5.0.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Workflow"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "4.0.0")
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
